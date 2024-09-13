c4b8bb17c1ef7d47b1851dbf0ebc4e1853864feb
Zalak <47916847+zalakpatel43@users.noreply.github.com>
2024-09-13 17:54:00 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# Define the URL for the health check
-HEALTH_CHECK_URL="http://localhost:80/health"
-
 echo "Waiting for the service to start..."
 sleep 10  # Wait for 10 seconds
 
-echo "Starting validation of the service..."
-
-# Send a request to the health check URL and capture the HTTP status code
+# Proceed with the health check
+HEALTH_CHECK_URL="http://localhost:80/health"
 HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" $HEALTH_CHECK_URL)
 
-# Check if the status code is 200 (OK)
 if [ "$HTTP_STATUS" -eq 200 ]; then
     echo "Service is running correctly. Health check passed."
     exit 0
