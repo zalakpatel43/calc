b64c764995cce4826e7c0124a458745746d51328
Zalak <47916847+zalakpatel43@users.noreply.github.com>
2024-09-16 16:51:37 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+# scripts/validate_service.sh
 
-echo "Waiting for the service to start..."
-sleep 10  # Wait for 10 seconds
+# Ensure this is the correct URL or endpoint your application should serve
+URL="http://localhost"
 
-# Proceed with the health check
-HEALTH_CHECK_URL="http://localhost:80/health"
-HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" $HEALTH_CHECK_URL)
+# Check if the service is running and returning a 200 OK status
+HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" $URL)
 
-if [ "$HTTP_STATUS" -eq 200 ]; then
-    echo "Service is running correctly. Health check passed."
-    exit 0
-else
-    echo "Service is not running correctly. Health check failed with status code: $HTTP_STATUS"
-    exit 1
+if [ "$HTTP_STATUS" -ne 200 ]; then
+  echo "Service is not running correctly. Health check failed with status code: $HTTP_STATUS"
+  exit 1
 fi
+
+echo "Service is running correctly."
+exit 0
